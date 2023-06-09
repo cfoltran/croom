8121c256fe778151e10d65e05cd54caf39b12602
Clément Foltran <clem@yuzu.green>
2023-06-09 16:06:44 +0200

    start home page

---
+++ lib/common/config/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF4C8086);
+  static const Color secondary = Color(0xFF336D75);
+  static const Color accent = Color(0xFF80A4A8);
+  static const Color background = Color(0xFFF5F7EE);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color error = Color(0xFFFB4E41);
+  static const Color success = Color(0xFF9CF1A3);
+  static const Color warning = Color(0xFFF79009);
+  static const Color textPrimary = Color(0xFF475467);
+  static const Color textSecondary = Color(0xFF667085);
+  static const Color textDisabled = Color(0xFF98A2B3);
+
+  static const MaterialColor materialPrimary = MaterialColor(
+    0xFF4C8086,
+    {
+      50: Color(0xFFE5EDEE),
+      100: Color(0xFFCCDBDC),
+      200: Color(0xFFB3C8CB),
+      300: Color(0xFF99B6BA),
+      400: Color(0xFF80A4A8),
+      500: Color(0xFF669297),
+      600: Color(0xFF4C8086),
+      700: Color(0xFF336D75),
+      800: Color(0xFF1A5B63),
+      900: Color(0xFF004952),
+    },
+  );
+}
